dbfe66d80a7f35efffff398787e1ae3176e64353
MuhammadAliGhaffar <alibrohi999@gmail.com>
2021-01-30 14:41:39 +0500

    Basic Array Declaration

---
+++ ArrayDemo/src/arraydemo/ArrayDemo.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraydemo;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Ali
+ */
+public class ArrayDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //ALT+SHIFT+F For formating code
+        int arr[];
+        arr = new int[10];
+
+        boolean bool[] = new boolean[10];
+
+        int arr1[] = {1, 2, 3};
+
+        int arr2[][] = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+        
+        System.out.println(Arrays.toString(arr1));
+        
+        int Array2D[][]=new int[5][5];
+        
+        
+    }
+
+}
